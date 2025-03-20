a639da9290da150da704b7735e3014ede09f2484
dmanish <mdahiya@gmail.com>
2025-03-20 07:13:33 +0000

    temp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
- using namespace std;
+#include<vector>
+using namespace std;
+#include <algorithm>
 
- vector<int> threSum(vector<int>& nums) {
-        vector<int> result;
+ vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
         if(nums.size() < 3) return result;
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() - 2; i++) {
@@ -11,9 +13,7 @@
             int k = nums.size() - 1;
             while(j < k) {
                 if(nums[i] + nums[j] + nums[k] == 0) {
-                    result.push_back(nums[i]);
-                    result.push_back(nums[j]);
-                    result.push_back(nums[k]);
+                    result.push_back({nums[i], nums[j], nums[k]});
                     while(j < k && nums[j] == nums[j + 1]) j++;
                     while(j < k && nums[k] == nums[k - 1]) k--;
                     j++;
@@ -22,6 +22,8 @@
                     j++;
                 } else {
                     k--;
+                    }
+                    return 0;
                 }
             }
         }
@@ -30,9 +32,9 @@
 
 int main() {
     vector<int> nums = {-1, 0, 1, 2, -1, -4};
-    vector<int> result = threSum(nums);
+    vector<vector<int>> result = threeSum(nums);
     for(int i = 0; i < result.size(); i++) {
-        cout << result[i] << " ";
+    vector<vector<int>> result = threeSum(nums);
+    for(int i = 0; i < result.size(); i++) {
+        cout << "[" << result[i][0] << ", " << result[i][1] << ", " << result[i][2] << "] ";
     }
-    return 0;
-}
